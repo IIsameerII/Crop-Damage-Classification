d18b37cba77146ca2e7c1ed6245c794634d3ff36
IIsameerII <sameerahamed300@gmail.com>
2024-01-18 01:03:50 -0330

    Adding Button functionality to change pages

--- pages/use_labeled_images.py
+++ pages/use_labeled_images.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
 st.write('labeled_images')
+
+
+
+
+
+
+
+
+with st.container():
+    st.header('Try our solution for prediction')
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button('Home'):
+            st.switch_page(r"main_page.py")
+    with col2:
+        if st.button('Use Custom Images'):
+            st.switch_page(r"pages/use_own_images.py")
--- pages/use_own_images.py
+++ pages/use_own_images.py
@@ -1,3 +1,25 @@
 import streamlit as st
 
 st.write('own_images')
+
+
+
+
+
+
+
+
+
+
+
+
+
+with st.container():
+    st.header('Try our solution for prediction')
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button('Home'):
+            st.switch_page(r"main_page.py")
+    with col2:
+        if st.button('Use Labeled Images'):
+            st.switch_page(r"pages/use_labeled_images.py")
