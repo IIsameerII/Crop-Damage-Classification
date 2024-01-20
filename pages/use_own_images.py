9d1f3ccfb79b2e962b1fce9a6302db4032af3fc5
IIsameerII <sameerahamed300@gmail.com>
2024-01-20 17:59:51 -0330

    Update use_own_images.py

--- pages/use_own_images.py
+++ pages/use_own_images.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import torch
 import torchvision
+from PIL import Image
 
 st.set_page_config(page_title='Crop Damage Classification',initial_sidebar_state='collapsed')
 
@@ -16,7 +17,48 @@ def load_crop_damage_model():
     model.eval()
     return model, automatic_transform, device, class_names
 
-model = load_crop_damage_model()
+def predict(model,X,transform,device,class_names):
+    # Transfrom the image
+    transformed_image = transform(X)
+    # Unsqueeze the image
+    pred_image = torch.unsqueeze(transformed_image,dim=0)
+    pred_image = pred_image.to(device)
+
+    with torch.inference_mode():
+        # Get logits for forward pass
+        y_logits = model(pred_image)
+
+        # Get pred
+        y_pred_prob = torch.argmax(y_logits,dim=1).item()
+
+        # Predicted class
+        predicted_class = class_names[y_pred_prob]
+
+    return predicted_class
+
+model, transform, device, class_names = load_crop_damage_model()
+
+# Get the image using file_uploader widget
+image = st.file_uploader(label='Upload an image of an agricultural field',
+                 accept_multiple_files=False)
+
+with st.spinner("Prediction Running...Please Wait.."):
+    if image!=None:
+
+        # Open the image
+        image = Image.open(image).convert('RGB')
+
+        # Display the image
+        st.image(image)
+
+        # Send the image for prediction
+        pred_class = predict(model,
+                                 image,
+                                 transform,
+                                 device,
+                                 class_names)
+
+        st.info(f'Predicted Class: {pred_class}')
 
 
 
